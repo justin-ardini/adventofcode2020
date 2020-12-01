96e4c80aa49a84ff9ec448471ff76da851874e9a
Justin Ardini <justin.ardini@gmail.com>
2020-11-30 19:06:00 -0500

    Finish package setup

---
+++ src/1.ts
@@ -0,0 +1,4 @@
+
+export async function solve(): Promise<number> {
+  return 1;
+}
---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import {solve} from './1';
+
+const app = express();
+
+app.get('/', async (_, res, next) => {
+  try {
+    const solution = await solve();
+    res.send('Solution: ' + solution);
+  } catch (error) {
+    return next(error);
+  }
+});
+
+app.listen(8000, () => {
+  console.log('App listening on http://localhost:8000')
+});
